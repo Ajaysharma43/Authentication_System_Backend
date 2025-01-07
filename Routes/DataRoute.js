1353f3504ece696e947f5e3088120449d08d99f7
ajay sharma <ajaysharma445446@gmail.com>
2025-01-07 16:46:46 +0530

    new changes for pagination

--- Routes/DataRoute.js
+++ Routes/DataRoute.js
@@ -6,8 +6,13 @@ const Users = require("../Schemma/UserSchemma")
 const app = express();
 
 Router.get('/UsersData', async(req,res)=>{
-    const UsersData = await Users.find()
-    res.send(UsersData)
+    const limit = parseInt(req.query.limit)
+    const page = parseInt(req.query.page)
+
+    const skip = (page - 1) * limit;
+    const UsersData = await Users.find().skip(skip).limit(limit)
+    const totalsongs = await Users.countDocuments();
+    res.json({UsersData,TotalPages:Math.ceil(totalsongs / limit)})
 });
 
 module.exports = Router;
